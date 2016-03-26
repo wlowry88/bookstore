cda39125e2001f872f47c971d4a72526ea800998
Will Lowry <william.c.lowry@gmail.com>
2016-03-26 14:39:43 -0700

    add global App variable and App.store

---
+++ app/instance-initializers/global.js
@@ -0,0 +1,10 @@
+export function initialize(application) {
+	application.store = application.lookup("service:store");
+	window.App = application;
+}
+
+export default {
+	name: 'global',
+	initialize: initialize
+};
+
--- app/models/publishing-house.js
+++ app/models/publishing-house.js
@@ -1,4 +1,3 @@
-import DS from 'ember-data';
 import Publisher from './publisher';
 
 export default Publisher.extend();
