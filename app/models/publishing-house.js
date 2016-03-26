5e5ef6ec492ce983b118fd80ac3a6b182d33c996
Will Lowry <william.c.lowry@gmail.com>
2016-03-26 14:17:58 -0700

    extend Publisher instead of using the DS.Model default

--- app/models/publishing-house.js
+++ app/models/publishing-house.js
@@ -1,7 +1,4 @@
 import DS from 'ember-data';
+import Publisher from './publisher';
 
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  discount: DS.attr('number'),
-  books: DS.hasMany('book')
-});
+export default Publisher.extend();
