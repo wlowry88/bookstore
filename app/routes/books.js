9528c241b4168817774449c1f45ccd8f0ec8d47e
Will Lowry <william.c.lowry@gmail.com>
2016-03-26 18:27:27 -0700

    generate modal and open / close

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,7 +1,26 @@
-import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model() {
-		return this.store.findAll('book');
-	}
+
+  model() {
+    return this.store.findAll('book');
+  },
+
+  actions: {
+    openCheckoutModal(book) {
+      return this.render('modal', {
+        outlet: 'modal',
+        into: 'application',
+        model: book,
+        controller: 'application'
+      });
+    },
+
+    closeCheckoutModal() {
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
+  }
+
 });
